5ab6f69481a003aacdefecd7ef8e7b901b8e4959
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-08 05:16:32 -0400

    commit tree 2c0b2b9c90cca3d1bfd698e4747c1003953435dc

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser = 'Leon'
+typeof "const currentUser = 'Leon';";
+"const currentUser = 'Leon';".length;
+currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+      
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
